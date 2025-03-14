fc046419cd3917f8a97c23cb6643bf895e06d7c3
Andrey Yurpalov <a.yurpalov@mail.ru>
2025-03-14 17:26:52 +0500

    modified Makefile build.sh

--- Makefile
+++ Makefile
@@ -1,5 +1,16 @@
 build:
 	./build.sh
 
+install:
+	uv pip install -r requirements.txt
+
+# Собираем статику
+collectstatic:
+	python3 manage.py collectstatic --noinput
+
+# Применяем миграции
+migrate:
+	python3 manage.py migrate
+
 render-start:
 	gunicorn task_manager.wsgi
--- build.sh
+++ build.sh
@@ -3,7 +3,4 @@
 curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
 
-# здесь добавьте все необходимые команды для установки вашего проекта
-# команду установки зависимостей, сборки статики, применения миграций и другие
-uv pip install -r requirements.txt
-#make install && make collectstatic && make migrate
+make install && make collectstatic && make migrate
