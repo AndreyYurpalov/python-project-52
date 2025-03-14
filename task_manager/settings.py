317deecafaf1176a6748dcd1a0346406fd0a8050
Andrey Yurpalov <a.yurpalov@mail.ru>
2025-03-14 19:04:17 +0500

    Modified .gitignore task_manager/settings.py

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-inj7-k3=&adykazl-61jb_(7ufo(^dige8(!k*u5@mh*82g6g_'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', False).lower() == 'true'
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
