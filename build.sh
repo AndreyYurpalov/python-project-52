7e4f6f3eedd264e90393f3ffef5e77dd86f1045c
Andrey Yurpalov <a.yurpalov@mail.ru>
2025-03-14 18:21:55 +0500

    Modified Makefile build.sh

--- build.sh
+++ build.sh
@@ -4,5 +4,18 @@
 curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
 
+source $HOME/.local/bin/env
+
+# Устанавливаем зависимости
+uv pip install -r requirements.txt
+
+# Собираем статику
+python manage.py collectstatic --noinput
+
+# Применяем миграции
+python manage.py migrate
+
+
+
 # Устанавливаем зависимости, собираем статику и применяем миграции
-make install && make collectstatic && make migrate
+# make install && make collectstatic && make migrate
